38578b9e19e7485431f24c8cc3007dbc000e4123
thakakhwa <zaidqal@hotmail.com>
2025-03-29 17:43:42 +0300

    fixedddd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import AddEvents from './pages/addEvents'; // Import the AddEvents page
 import FAQ from "./pages/FAQ"
 import TOS from "./pages/TOS"
 import ContactUs from "./pages/ContactUs"
-import Privacy from "./pages/privacypolicy"
+import Privacypolicy from "./pages/Privacypolicy"
 
 
 const App = () => {
@@ -90,7 +90,7 @@ const App = () => {
           <Route path="/tos" element={<TOS />} />
           <Route path="/faq" element={<FAQ />} />
           <Route path="/contact" element={<ContactUs />} />
-          <Route path="/Privacy" element={<privacy />} />
+          <Route path="/Privacy" element={<Privacypolicy />} />
 
           Protected User Dashboard
           <Route 
